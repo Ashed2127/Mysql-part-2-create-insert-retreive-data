25381528987ad4b611f3f94f76d8ee826a98a5d5
unknown <ashebirdiriba8888@gmail.com>
2023-11-29 14:22:15 +0000

    submit update delete are work

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 const mysql = require("mysql"); // Import the MySQL module for database connection
 const express = require("express"); // Import the Express module for creating a web server
 const bodyParser = require("body-parser");
-const express = require("express");
-const jquery = require("jquery");
+// const jquery = require("jquery");
 
 // Create an Express app instance
 const app = express();
@@ -41,13 +40,13 @@ myConnection.connect((err) => {
 app.get("/createtable", (req, res) => {
   // Define the SQL queries to create the tables
   let products = `CREATE TABLE IF NOT EXISTS Products(
-    product_id INT(11) NOT NULL,
+    product_id INT(11) AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (product_id)
   )`;
 
   let company = `CREATE TABLE IF NOT EXISTS Company(
-    product_id INT(11),
+    product_id INT(11) AUTO_INCREMENT NOT NULL,
     company_name VARCHAR(255) NOT NULL,
     FOREIGN KEY (product_id) REFERENCES Products (product_id)
   )`;
@@ -118,8 +117,8 @@ app.get("/getData", (req, res) => {
     res.send(rows);
     //to append the rows value to the the table
     columns = document.getElementsByTagName("td");
+  });
 });
-
 //to select product name from products table
 //to select company name from company table
 app.get("/getEachData", (req, res) => {
@@ -130,43 +129,61 @@ LEFT JOIN company ON products.product_id = company.product_id`;
   myConnection.query(getEachData, (err, rows, fields) => {
     if (err) {
       console.log("Error ", err);
-    } 
+    }
     res.send(rows);
   });
 });
 
 // //to update tables
-// // app.use(bodyParser.json());
 
-// app.post("/updateName", (req, res) => {
-//   const { product_id, product_name } = req.body;
+app.post("/updateName", (req, res) => {
+  const { product_id, product_name } = req.body;
 
-//   let updateName = `UPDATE products SET product_name = '${product_name}' WHERE product_id = ${product_id}`;
+  let updateName = `UPDATE products SET product_name = '${product_name}' WHERE product_id = ${product_id}`;
+  myConnection.query(updateName, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error updating product name");
+      return;
+    }
+    if (product_id) {
+      console.log("enter valid id");
+    }
+    res.send("Product name updated successfully");
+  });
+});
 
-//   myConnection.query(updateName, (err, rows) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send("Error updating product name");
-//       return;
-//     }
-//     if (product_id) {
-//       console.log("enter valid id");
-//     }
-//     res.send("Product name updated successfully");
-//   });
+// //to update product id using product name
+// let updateId = `UPDATE products SET product_name = ${product_name} WHERE product_id = ${product_id}`;
+// myConnection.query(updateId, (err, rows) => {
+//   if (err) {
+//     console.log(err);
+//     res.status(500).send("Error updating product name");
+//     return;
+//   }
+//   if (product_name) {
+//     console.log("enter valid name");
+//   }
+//   res.send("Product id updated successfully");
 // });
 
-// //to delete the product
-// // app.post("/deleteName", (req, res) => {
-// //   const { product_id } = req.body;
+// to delete the product
+app.post("/deleteName", (req, res) => {
+  const { product_id } = req.body;
 
-// //   let updateName = `DELETE FROM products JOIN company JOIN description WHERE product_id = ${product_id}`;
+  let deleteCompany = `DELETE FROM company WHERE product_id = ${product_id}`;
+  let deleteProduct = `DELETE FROM products WHERE product_id = ${product_id}`;
+  //the company data must deleted first b/c prodct_id is foreign key(foreign key data 1st deleted)
+  myConnection.query(deleteCompany, (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 
-// //   myConnection.query(updateName, (err, rows) => {
-// //     if (err) {
-// //       console.log(err);
-// //     }
-
-// //     res.send("Product Deleted successfully");
-// //   });
-// // });
+  myConnection.query(deleteProduct, (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send("Product Deleted successfully");
+  });
+});
